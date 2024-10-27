6c8dc4b7cb3f8dddae13b0ed5ba38a8b9ba48580
Felipe-candido <145620975+Felipe-candido@users.noreply.github.com>
2024-10-27 00:56:38 -0300

    adaptando o registro usando ModelForm no projeto_experimental3

---
+++ projeto_experimental3/enpowernet/core/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import include, path
+from . import views
+from .views import add_usuario, realizar_login, logout, add_usuario2
+
+urlpatterns = [
+    path('', views.home),
+    path('registro/', add_usuario2, name='add_usuario2'),
+    # path('registro/', add_usuario, name='add_usuario'),
+    path('login/', realizar_login, name='realizar_login'),
+    path("logout/", logout, name="logout"),
+]
