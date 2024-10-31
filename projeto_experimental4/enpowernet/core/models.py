39a7e1830846c7650ef36ee8ff3943fcc75f44bd
Jrdotan <darlanj207@gmail.com>
2024-10-31 02:18:31 -0300

    v2.4- UUID

--- projeto_experimental4/enpowernet/core/models.py
+++ projeto_experimental4/enpowernet/core/models.py
@@ -1,3 +1,5 @@
+import uuid
+from bson.binary import Binary
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.db import models
 from dateutil.relativedelta import relativedelta
@@ -9,8 +11,8 @@ class UsuarioManager(BaseUserManager):
             raise ValueError('O email deve ser fornecido')
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
-        user.set_password(password)  # Basicamente pra evitar problemas 
-        user.save(using=self._db)    # Com hashing eu tive que criar essa classe abstrata pra isso
+        user.set_password(password)
+        user.save(using=self._db)
         return user
 
     def create_superuser(self, email, password=None, **extra_fields):
@@ -24,7 +26,8 @@ class usuario(AbstractBaseUser):
         ('M', 'Masculino'),
         ('I', 'Indefinido'),  
     )
-    email = models.EmailField(primary_key=True)
+    UUID = models.BinaryField(primary_key=True, editable=False) 
+    email = models.EmailField(unique=True)
     nome = models.CharField(max_length=255)
     genero = models.CharField(max_length=1, choices=escolha_genero)
     telefone = models.CharField(max_length=11)
@@ -44,3 +47,8 @@ class usuario(AbstractBaseUser):
 
     def __str__(self):
         return self.nome  
+
+    def save(self, *args, **kwargs):
+        if not self.UUID:
+             self.UUID = Binary(uuid.uuid4().bytes)
+        super().save(*args, **kwargs)
