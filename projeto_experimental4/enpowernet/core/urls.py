ca02bf1638f9d1a21e2a28fec856e7a3005712f9
Felipe-candido <145620975+Felipe-candido@users.noreply.github.com>
2024-11-03 18:31:57 -0300

    criei um formulario para editar perfil

--- projeto_experimental4/enpowernet/core/urls.py
+++ projeto_experimental4/enpowernet/core/urls.py
@@ -6,7 +6,6 @@ from .views import  realizar_login, logout, add_usuario2, perfil
 urlpatterns = [
     path('', views.home, name='home'),
     path('registro/', add_usuario2, name='add_usuario2'),
-    # path('registro/', add_usuario, name='add_usuario'),
     path('login/', realizar_login, name='realizar_login'),
     path("logout/", logout, name="logout"),
     path("perfil/", perfil, name="perfil"),
